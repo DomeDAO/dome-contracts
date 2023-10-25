b40d90d6ea57ae1d2c5a26fd3f0e07f1356f3aa3
va <mrsmeddy@gmail.com>
2023-10-25 16:30:41 +0400

    feat: add wrapped voting factory

---
+++ contracts/WrappedVotingFactory.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {DomeWrappedVoting} from "./WrappedVoting.sol";
+
+contract WrappedVotingFactory {
+	event WrappedVotingCreated(address token, address wrappedVoting);
+
+	function createWrapper(
+		address token
+	) external returns (address wrappedVoting) {
+		wrappedVoting = address(new DomeWrappedVoting(token));
+
+		emit WrappedVotingCreated(token, wrappedVoting);
+	}
+}
