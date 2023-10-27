973965a0da9d3dd10177a53c1838059f7f6caa20
va <mrsmeddy@gmail.com>
2023-10-27 10:24:18 +0400

    feat: ownable, updatable addresses for price tracker for owner

--- contracts/PriceTracker.sol
+++ contracts/PriceTracker.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 interface IUniswapV2Router02 {
 	function getAmountsOut(
 		uint amountIn,
@@ -10,15 +12,53 @@ interface IUniswapV2Router02 {
 	function WETH() external pure returns (address);
 }
 
-contract PriceTracker {
-	address immutable UNISWAP_ROUTER;
-	address immutable USDC;
+contract PriceTracker is Ownable {
+	address UNISWAP_ROUTER;
+	address USDC;
 
 	error Unauthorized();
 
 	constructor(address _uniswapRouter, address _usdcAddress) {
 		UNISWAP_ROUTER = _uniswapRouter;
 		USDC = _usdcAddress;
+	}
+
+	function updateAddresses(
+		address _uniswapRouter,
+		address _usdc
+	) external onlyOwner {
+		UNISWAP_ROUTER = _uniswapRouter;
+		USDC = _usdc;
+	}
+
+	function convertTo(
+		address target,
+		address asset,
+		uint256 amount
+	) external view returns (uint256) {
+		if (asset == USDC) {
+			return amount;
+		}
+
+		address[] memory wethRoutedPath;
+		wethRoutedPath[0] = asset;
+		wethRoutedPath[1] = IUniswapV2Router02(UNISWAP_ROUTER).WETH();
+		wethRoutedPath[2] = target;
+
+		uint256[] memory wethRoutedAmounts = IUniswapV2Router02(UNISWAP_ROUTER)
+			.getAmountsOut(amount, wethRoutedPath);
+
+		address[] memory directPath;
+		directPath[0] = asset;
+		directPath[1] = target;
+
+		uint256[] memory directAmounts = IUniswapV2Router02(UNISWAP_ROUTER)
+			.getAmountsOut(amount, directPath);
+
+		return
+			directAmounts[1] > wethRoutedAmounts[2]
+				? directAmounts[1]
+				: wethRoutedAmounts[2];
 	}
 
 	function convertToUSDC(
