9bdfc3936420f04381d3fd4425be498270011f36
va <mrsmeddy@gmail.com>
2023-10-27 10:25:11 +0400

    feat: update verification scripts

--- scripts/verifyDome.js
+++ scripts/verifyDome.js
@@ -1,8 +1,10 @@
 const { ethers, run } = require("hardhat");
+const { getDomeEnvVars, getEnvVars } = require("../config");
+
+const { DOME_PROTOCOL_ADDRESS } = getDomeEnvVars();
+getEnvVars("POLYGON_API_KEY");
 
 async function main() {
-	const domeAddress = "0x...0";
-
 	const domeInfo = {
 		CID: "<DOME_CID>",
 		tokenName: "<DOME_TOKEN_NAME>",
@@ -23,7 +25,7 @@ async function main() {
 	const systemOwnerPercentage = "<SYSTEM_OWNER_PERCENTAGE>";
 	const domeProtocolAddress = "<DOME_PROTOCOL_ADDRESS>";
 
-	const dome = await ethers.getContractAt("Dome", domeAddress);
+	const dome = await ethers.getContractAt("Dome", DOME_PROTOCOL_ADDRESS);
 
 	const constructorArguments = dome.interface.encodeDeploy(
 		domeInfo,
