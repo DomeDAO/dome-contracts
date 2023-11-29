2a0274f4e816bda537ce4ed3d1f039d8c1c4d4b5
va <mrsmeddy@gmail.com>
2023-11-29 17:06:38 +0400

    ref: update verification script

--- scripts/verifyLatestDomeProtocol.js
+++ scripts/verifyLatestDomeProtocol.js
@@ -3,62 +3,14 @@ const { getLatestProtocolDeploy } = require("./utils");
 
 async function main() {
 	const network = await ethers.provider.getNetwork();
-	const {
-		DOME_PROTOCOL,
-		DOME_FACTORY,
-		GOVERNANCE_FACTORY,
-		WRAPPEDVOTING_FACTORY,
-		PRICE_TRACKER,
-		BUFFER,
-		REWARD_TOKEN,
-	} = getLatestProtocolDeploy(network.name);
+	const deployment = getLatestProtocolDeploy(network.name);
 
-	const domeProtocol = await ethers.getContractAt(
-		"DomeProtocol",
-		DOME_PROTOCOL.address
-	);
-
-	const domeFactoryAddress = await domeProtocol.callStatic.DOME_FACTORY();
-	const governanceFactoryAddress =
-		await domeProtocol.callStatic.GOVERNANCE_FACTORY();
-	const wrappedVotingFactoryAddress =
-		await domeProtocol.callStatic.WRAPPEDVOTING_FACTORY();
-	const priceTrackerAddress = await domeProtocol.callStatic.PRICE_TRACKER();
-
-	await run("verify:verify", {
-		address: domeProtocol.address,
-		constructorArguments: DOME_PROTOCOL.constructorArguments,
-	});
-
-	await run("verify:verify", {
-		address: domeFactoryAddress,
-		constructorArguments: DOME_FACTORY.constructorArguments,
-	});
-
-	await run("verify:verify", {
-		address: governanceFactoryAddress,
-		constructorArguments: GOVERNANCE_FACTORY.constructorArguments,
-	});
-
-	await run("verify:verify", {
-		address: wrappedVotingFactoryAddress,
-		constructorArguments: WRAPPEDVOTING_FACTORY.constructorArguments,
-	});
-
-	await run("verify:verify", {
-		address: priceTrackerAddress,
-		constructorArguments: PRICE_TRACKER.constructorArguments,
-	});
-
-	await run("verify:verify", {
-		address: BUFFER.address,
-		constructorArguments: BUFFER.constructorArguments,
-	});
-
-	await run("verify:verify", {
-		address: REWARD_TOKEN.address,
-		constructorArguments: REWARD_TOKEN.constructorArguments,
-	});
+	for (const key of Object.keys(deployment)) {
+		await run("verify:verify", {
+			address: deployment[key].address,
+			constructorArguments: deployment[key].constructorArguments,
+		});
+	}
 }
 
 main().catch((error) => {
